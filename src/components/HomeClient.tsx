7b69d06a6f876df670c49e28ca7fc6c9df71747e
Oumou Gory <oumougory@macbookpro.home>
2026-04-01 12:38:33 +0200

    UI: Hidden Opérations & Systèmes section on homepage

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -38,10 +38,12 @@ export default function HomeClient({ services, allTags }: { services: ServiceRec
               <ServiceRow title="Finance - Juridique" services={financeServices} />
             )}
 
-            {/* 2. Scrolling Row for Operations & Systems */}
+            {/* 2. Scrolling Row for Operations & Systems (Hidden as requested) */}
+            {/* 
             {otherServices.length > 0 && (
               <ServiceRow title="Opérations - Systèmes" services={otherServices} />
             )}
+            */}
           </>
         )}
       </div>
