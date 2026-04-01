70cddd4beadad0681a4f468862e75761f47e8799
Oumou Gory <oumougory@macbookpro.home>
2026-04-01 13:01:48 +0200

    Analytics: Updated import to @vercel/analytics/next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   variable: "--font-poppins",
