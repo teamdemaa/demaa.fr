97bc45e43cfb3b99ba867554833f71b98b6cb906
Oumou Gory <oumougory@macbookpro.home>
2026-04-08 15:26:22 +0200

    Add marketing tracking tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
@@ -18,6 +19,41 @@ export default function RootLayout({
   return (
     <html lang="fr" className="h-full antialiased">
       <body className="min-h-full flex flex-col bg-background text-foreground font-sans pb-24 md:pb-0">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V1V4EX55K6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics-base" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V1V4EX55K6');
+          `}
+        </Script>
+        <Script id="meta-pixel-base" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3030409753790915');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=3030409753790915&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <Footer />
         <Analytics />
